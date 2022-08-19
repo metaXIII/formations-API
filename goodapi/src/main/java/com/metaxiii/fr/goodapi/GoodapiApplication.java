b2e5b21f6e9f502f2b01a03cdfde199e5513c0ce
meta <lehchibi.gael@pm.me>
2022-08-19 12:20:28 +0200

    feat: start good API

---
+++ goodapi/src/main/java/com/metaxiii/fr/goodapi/GoodapiApplication.java
@@ -0,0 +1,13 @@
+package com.metaxiii.fr.goodapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoodapiApplication.class, args);
+    }
+
+}
---
+++ goodapi/src/test/java/com/metaxiii/fr/goodapi/GoodapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metaxiii.fr.goodapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
