f5363597a0ff30ee58c04a0e2b5a0d9867212fdc
metaxiii <lehchibi.gael@pm.me>
2022-08-22 23:58:09 +0200

    feat: remove import

--- goodapi/src/main/java/com/metaxiii/fr/goodapi/controller/EmployeeController.java
+++ goodapi/src/main/java/com/metaxiii/fr/goodapi/controller/EmployeeController.java
@@ -25,7 +25,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.plugin.core.PluginRegistry;
 import org.springframework.validation.DataBinder;
-import org.springframework.validation.Validator;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
