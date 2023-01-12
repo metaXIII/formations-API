4762f0d8929a0f27b921405d1dd692606069788e
metaxiii <lehchibi.gael@pm.me>
2023-01-12 11:31:04 +0100

    fix hateoas

--- goodAPI/src/main/java/com/metaxiii/fr/goodapi/transformer/DTOCreator.java
+++ goodAPI/src/main/java/com/metaxiii/fr/goodapi/transformer/DTOCreator.java
@@ -1,5 +1,5 @@
 package com.metaxiii.fr.goodapi.transformer;
 
-public interface DTOCreator<I, D> {
-  public I toDomain(final D employeeDTO);
+interface DTOCreator<I, D> {
+  I toDomain(final D employeeDTO);
 }
